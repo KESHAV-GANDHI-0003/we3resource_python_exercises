41ed4a1f466777e38c01bb614ef444f39fbe2832
Keshav Gandhi <keshav.gandhi0003@gmail.com>
2023-04-19 19:54:44 +0530

    Commited

--- ques_3.py
+++ ques_3.py
@@ -3,5 +3,7 @@ Sample Output :
 Current date and time :
 2014-07-05 14:34:14'''
 import datetime
-dt = datetime.datetime.now()
-print(f"Current Date & Time- {dt}")
+current_date_time = datetime.datetime.now()
+current_date = current_date_time.strftime("%d/%m/%Y")
+current_time = current_date_time.strftime("%H:%M:%S")
+print(f"Current date is {current_date} and time is {current_time}")
