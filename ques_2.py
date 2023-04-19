ccc6dff4a0892b6a70a1d3e50d360fa8e5adb759
Keshav Gandhi <keshav.gandhi0003@gmail.com>
2023-04-19 12:05:14 +0530

    Commited

---
+++ ques_2.py
@@ -0,0 +1,4 @@
+'''Ques :- Write a Python program to find out what version of Python you are using. '''
+import sys
+print(f"Python version - {sys.version}")
+print(f"Python version info - {sys.version_info}")
