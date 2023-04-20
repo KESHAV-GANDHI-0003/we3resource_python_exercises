dd7b5bb95d8ccb7da1f947694e3d3bcacab7e6ee
Keshav Gandhi <keshav.gandhi0003@gmail.com>
2023-04-20 18:03:44 +0530

    Commited

--- ques_4.py
+++ ques_4.py
@@ -1,1 +1,6 @@
-'''dflcdflk'''
+''' Write a Python program that calculates the area of a circle based on the radius entered by the user.'''
+from math import  pi
+print("Circle Area Calculator")
+radius = eval(input("Enter the radius of the Circle :- "))
+area_of_circle = pi*(radius**2)
+print(f"Area of Cirlce with radius {radius} is :- {area_of_circle}")
