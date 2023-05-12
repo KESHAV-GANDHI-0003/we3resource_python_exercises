0509190a79b3e2b35b38ba8a5e792cea363e0cf6
Keshav Gandhi <gandhi.keshav0007@gmail.com>
2023-05-12 19:07:17 +0530

    Commited

--- Python Programming Puzzles/ques_1.py
+++ Python Programming Puzzles/ques_1.py
@@ -13,10 +13,23 @@ Input:
 Output:
 True'''
 # Method - 1
-list_1 = eval(input("Enter the List using [] :- "))
+'''list_1 = eval(input("Enter the List using [] :- "))
 count_19 = list_1.count(19)
 count_5 = list_1.count(5)
 if count_19 == 2 and count_5 == 3:
     print("True")
 else:
+    print("False")'''
+# Method - 2
+list_1 = eval(input("Enter the List using [] :- "))
+for i in list_1:
+    if i == 19:
+        count_19 += 1
+    elif i == 5:
+        count_5 += 1
+print(count_19)
+print(count_5)
+if count_19 == 2 and count_5 == 3:
+    print("True")
+else: 
     print("False")
