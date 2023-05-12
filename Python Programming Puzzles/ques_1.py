f58c3fd7d4f1cac8d6a90bef14d6150c98a21ad9
Keshav Gandhi <gandhi.keshav0007@gmail.com>
2023-05-12 18:39:18 +0530

    Commited

---
+++ Python Programming Puzzles/ques_1.py
@@ -0,0 +1,16 @@
+'''Write a Python program to find a list of integers with exactly two occurrences of nineteen and
+at least three occurrences of five. Return True otherwise False.
+Input:
+[19, 19, 15, 5, 3, 5, 5, 2]
+Output:
+True
+Input:
+[19, 15, 15, 5, 3, 3, 5, 2]
+Output:
+False
+Input:
+[19, 19, 5, 5, 5, 5, 5]
+Output:
+True'''
+# Method - 1
+list_1 = eval(input("Enter the List using [] :- "))
